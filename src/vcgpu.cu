9d172497e154900eaba59ac9a4e15ac29a6cd6de
GregorySchwing <gschwing@uno.edu>
2022-05-16 15:15:13 -0500

    t

--- src/vcgpu.cu
+++ src/vcgpu.cu
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "vcgpu.h"
+#include "cub/cub.cuh"
 
 void VCGPU::findCover(int nrVertices, int threadsPerBlock, int *dforwardlinkedlist, int *dbackwardlinkedlist, int *dmatch, int *dlength, int *dheadlist, int *dheadbool)
 {
