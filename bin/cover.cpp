b49f8f69c2be19cb22cf3c2ca6165b72d4382738
greg <go2432@wayne.edu>
2022-05-27 18:23:46 -0500

    pass dev pter

--- bin/cover.cpp
+++ bin/cover.cpp
@@ -528,7 +528,8 @@ int main(int argc, char **argv)
 					//GraphMatching *matcher = getMatcher(graph2, *i, GPUNrThreadsPerBlock, barrier);
 					
 					match = vc.matcher.initialMatching();
-					vc.matcher.performMatching(match, t1, t2, fll, bll, lengthOfPath, &degrees[0], &edgestatus[0]);
+					//vc.matcher.performMatching(match, t1, t2, fll, bll, lengthOfPath, &degrees[0], &edgestatus[0]);
+					vc.matcher.performMatching(match, t1, t2, fll, bll, lengthOfPath, vc.matcher.ddegrees, vc.matcher.dedgestatus);
 
 					//delete matcher;
 				}
