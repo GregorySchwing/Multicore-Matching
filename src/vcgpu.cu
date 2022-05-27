9b2ba8a9f3fe4e5377ef126c6fb35195bfe40bc2
greg <go2432@wayne.edu>
2022-05-27 16:47:49 -0500

    t

--- src/vcgpu.cu
+++ src/vcgpu.cu
@@ -23,6 +23,10 @@ using namespace mtc;
 //==== Kernel variables ====
 __device__ int dkeepMatching;
 
+texture<int2, cudaTextureType1D, cudaReadModeElementType> neighbourRangesTexture;
+texture<int, cudaTextureType1D, cudaReadModeElementType> neighboursTexture;
+texture<float, cudaTextureType1D, cudaReadModeElementType> weightsTexture;
+
 VCGPU::VCGPU(const Graph &_graph, const int &_threadsPerBlock, const unsigned int &_barrier) :
 		graph(_graph),
         threadsPerBlock(_threadsPerBlock),
