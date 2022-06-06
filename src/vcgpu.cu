ca50d067900819742ecf03088b10789530464aad
greg <go2432@wayne.edu>
2022-06-06 16:42:22 -0500

    a

--- src/vcgpu.cu
+++ src/vcgpu.cu
@@ -15,6 +15,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "vcgpu.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
 //#include "cub/cub.cuh"
 
 using namespace std;
