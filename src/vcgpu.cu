75a665ac818aae843c23bc88b2b92b7f7c012c38
greg <go2432@wayne.edu>
2022-06-13 12:33:20 -0500

    t

--- src/vcgpu.cu
+++ src/vcgpu.cu
@@ -74,14 +74,14 @@ VCGPU::VCGPU(const Graph &_graph, const int &_threadsPerBlock, const unsigned in
 	neighbourRangesTexture.addressMode[0] = cudaAddressModeWrap;
 	neighbourRangesTexture.filterMode = cudaFilterModePoint;
 	neighbourRangesTexture.normalized = false;
-	cudaBindTexture(0, neighbourRangesTexture, (void *)dneighbourRanges, neighbourRangesTextureDesc, sizeof(int2)*graph.neighbourRanges.size());
+	cudaBindTexture(0, neighbourRangesTexture, (void *)matcher.dneighbourRanges, neighbourRangesTextureDesc, sizeof(int2)*graph.neighbourRanges.size());
 	
 	cudaChannelFormatDesc neighboursTextureDesc = cudaCreateChannelDesc<int>();
 
 	neighboursTexture.addressMode[0] = cudaAddressModeWrap;
 	neighboursTexture.filterMode = cudaFilterModePoint;
 	neighboursTexture.normalized = false;
-	cudaBindTexture(0, neighboursTexture, (void *)dneighbours, neighboursTextureDesc, sizeof(int)*graph.neighbours.size());
+	cudaBindTexture(0, neighboursTexture, (void *)matcher.dneighbours, neighboursTextureDesc, sizeof(int)*graph.neighbours.size());
 
 
 }
