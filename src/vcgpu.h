d5dc6f3ced826712b0431a1585aa14063c6a1efe
greg <go2432@wayne.edu>
2022-06-15 12:54:40 -0500

    include

--- src/vcgpu.h
+++ src/vcgpu.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "graph.h"
 #include "matchgpu.h"
 #include <exception>
-
+#include <memory>
 // RE?
 #include <ncurses.h>
 
