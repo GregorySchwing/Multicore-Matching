380352a52cfc3ca6005f92589e119deec6572550
greg <go2432@wayne.edu>
2022-06-15 18:57:59 -0500

    a

--- utils/GraphViz.h
+++ utils/GraphViz.h
@@ -17,8 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef GRAPHVIZ_H
 #define GRAPHVIZ_H
 
-#include "../DotWriter/lib/DotWriter.h"
-#include "../DotWriter/lib/Enums.h"
+#include "DotWriter/lib/DotWriter.h"
+#include "DotWriter/lib/Enums.h"
 #include <set>
 #include <sstream>
 
