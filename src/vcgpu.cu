19f54eecef810726239d9fa18e75c12d261cf634
greg <go2432@wayne.edu>
2022-06-15 12:28:15 -0500

    wtf

--- src/vcgpu.cu
+++ src/vcgpu.cu
@@ -65,7 +65,7 @@ VCGPU::VCGPU(const Graph &_graph, const int &_threadsPerBlock, const unsigned in
     //cudaMalloc(&dedgestatus, sizeof(int)*graph.nrEdges) != cudaSuccess || 
     if (cudaMalloc(&dedgestatus, sizeof(int)*graph.neighbours.size()) != cudaSuccess || 
         cudaMalloc(&dlength, sizeof(int)*graph.nrVertices) != cudaSuccess || 
-        cudaMallocManaged(&dsearchtree, sizeof(int2)*sizeOfSearchTree) != cudaSuccess || 
+        cudaMalloc(&dsearchtree, sizeof(int2)*sizeOfSearchTree) != cudaSuccess || 
         cudaMalloc(&dfullpathcount, sizeof(int)*1) != cudaSuccess || 
         cudaMalloc(&dnumleaves, sizeof(int)*1) != cudaSuccess || 
         cudaMalloc(&dfinishedLeavesPerLevel, sizeof(float)*depthOfSearchTree) != cudaSuccess || 
@@ -75,6 +75,7 @@ VCGPU::VCGPU(const Graph &_graph, const int &_threadsPerBlock, const unsigned in
 		cerr << "Not enough memory on device!" << endl;
 		throw exception();
 	}
+    printf("finished cudamallocing\n");
     edgestatus = new int[graph.neighbours.size()];
     newdegrees = new int[graph.nrVertices];
     ReinitializeArrays();
