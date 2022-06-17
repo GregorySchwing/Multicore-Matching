16dac89390f0b4db89d1ec80c38a0de3339e665f
greg <go2432@wayne.edu>
2022-06-17 13:37:43 -0500

    ow

--- utils/GraphViz.h
+++ utils/GraphViz.h
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#include "../DotWriter/lib/Enums.h"
 #include <set>
 #include <sstream>
+#include <string>
+#include "graph.h"
 
 class GraphViz {
     public:
