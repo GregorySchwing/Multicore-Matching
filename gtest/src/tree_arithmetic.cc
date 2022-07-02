90c4ce5c5708db1dca8690f37249300ac316e04f
greg <go2432@wayne.edu>
2022-07-02 13:37:29 -0400

    u[date

--- gtest/src/tree_arithmetic.cc
+++ gtest/src/tree_arithmetic.cc
@@ -7,10 +7,12 @@ TEST(DeepgreenMatrix, BasicConstructor) {
   int depthOfSearchTree = 10;
   long long sizeOfSearchTree = CalculateSpaceForDesiredNumberOfLevels(depthOfSearchTree);
   std::vector<int> searchTree(sizeOfSearchTree);
-  
+  RecursivelyCallFillTree(0,sizeOfSearchTree);
+  /*
   std::array<int, 4> myArray = CalculateLeafOffsets(3,1);
   for (auto & a : myArray)
     std::cout <<  a << " ";
+  */
   EXPECT_FLOAT_EQ(11.0f, 11.0f);
 
 }
